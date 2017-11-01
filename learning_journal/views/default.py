7a670671f5c213f960b8418264997aa70595505a
Matt Favoino <mattfavoino@gmail.com>
2017-10-31 20:52:37 -0700

    Trying to deploy to heroku

--- learning_journal/views/default.py
+++ learning_journal/views/default.py
@@ -12,25 +12,25 @@ def list_view(request):
     """Serve all journal entries on home page."""
     path = os.path.join(HERE, '../templates/index.html')
     with io.open(path) as file:
-        return Response(file.read())  # this is correct
+        return Response(file.read())
 
 
 def detail_view(request):
     """Serve detail HTML page for a single entry."""
     path = os.path.join(HERE, '../templates/detail.html')
     with io.open(path) as file:
-        return Response(file.read())  # this is correct
+        return Response(file.read())
 
 
 def create_view(request):
     """Serve create entry HTML page."""
     path = os.path.join(HERE, '../templates/new.html')
     with io.open(path) as file:
-        return Response(file.read())  # this is correct
+        return Response(file.read())
 
 
 def update_view(request):
     """Update existing journal entry."""
     path = os.path.join(HERE, '../templates/edit.html')
     with io.open(path) as file:
-        return Response(file.read())  # this is correct
+        return Response(file.read())

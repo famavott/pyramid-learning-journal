b91de2c6dc2eecf83453c2b7a4e96d0b03893f1e
Matt Favoino <mattfavoino@gmail.com>
2017-11-01 16:51:02 -0700

    Restructuring default.py and __init__

--- learning_journal/__init__.py
+++ learning_journal/__init__.py
@@ -1,13 +1,11 @@
+"""Init function to include routes and jinja2 files to make app."""
 from pyramid.config import Configurator
 
 
 def main(global_config, **settings):
-    """ This function returns a Pyramid WSGI application.
-    """
+    """Function returns a Pyramid WSGI application."""
     config = Configurator(settings=settings)
     config.include('pyramid_jinja2')
-    config.include('.models')
     config.include('.routes')
-    config.include('.views')
     config.scan()
     return config.make_wsgi_app()

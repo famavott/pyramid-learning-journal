8f3c3a06d37123d767d9c4817549289eb7adf588
Matt Favoino <mattfavoino@gmail.com>
2017-11-01 19:51:17 -0700

    Added sample entries in data dir

---
+++ data/entires.py
@@ -0,0 +1,8 @@
+"""List of journal entries."""
+
+ENTRIES = [
+    {'id': 1, 'author': 'Matt Favoino', 'title': 'Day 1', 'date': '10/16/2017', 'text': 'yay text!'},
+    {'id': 2, 'author': 'Matt Favoino', 'title': 'Day 2', 'date': '10/17/2017', 'text': 'yay text1!'},
+    {'id': 3, 'author': 'Matt Favoino', 'title': 'Day 3', 'date': '10/18/2017', 'text': 'yay text2!'},
+    {'id': 4, 'author': 'Matt Favoino', 'title': 'Day 4', 'date': '10/19/2017', 'text': 'yay text3!'},
+]

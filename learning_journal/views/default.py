c9b0edd4f555887c6a17cd938abf010eb4860367
Matt Favoino <mattfavoino@gmail.com>
2017-11-01 19:51:50 -0700

    Saved edit jinja page and default.py

--- learning_journal/views/default.py
+++ learning_journal/views/default.py
@@ -6,22 +6,22 @@ from pyramid.view import view_config
 @view_config(route_name='home', renderer='learning_journal:templates/index.jinja2')
 def list_view(request):
     """Pass response to send to index.html page."""
-    pass
+    return {}
 
 
 @view_config(route_name='detail', renderer='learning_journal:templates/detail.jinja2')
 def detail_view(request):
     """Pass response to send to detail.html page."""
-    pass
+    return {}
 
 
 @view_config(route_name='create', renderer='learning_journal:templates/new.jinja2')
 def create_view(request):
     """Pass response to send to new.html page."""
-    pass
+    return {}
 
 
 @view_config(route_name='edit', renderer='learning_journal:templates/edit.jinja2')
 def update_view(request):
     """Pass response to send to edit.html page."""
-    pass
+    return {}
